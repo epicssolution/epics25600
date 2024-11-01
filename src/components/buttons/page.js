1137bb6d3b97cc667afa0a2476fa01d0fb2ea47a
epicssolution <dharti25600@gmail.com>
2024-11-01 22:22:27 +0500

    Update page.js

--- src/components/buttons/page.js
+++ src/components/buttons/page.js
@@ -6,7 +6,8 @@ export default function VisitCourseButton({ href }) {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className=" bg-accent dark:bg-accentDark text-light dark:text-dark font-semibold transition-colors duration-300"
+      type="button"
+      className=" bg-accent dark:bg-accentDark text-light dark:text-dark py-2 text-light dark:text-dark font-semibold transition-colors  duration-300"
     >
       Click Here to Get Course
     </a>
