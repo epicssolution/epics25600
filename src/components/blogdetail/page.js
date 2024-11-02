30392e38ea0b4ceb70424c6c21c1d5a396d03300
epicssolution <dharti25600@gmail.com>
2024-11-02 13:41:51 +0500

    Update page.js

--- src/components/blogdetail/page.js
+++ src/components/blogdetail/page.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const BlogDetails = ({ blog }) => {
   return (
-    <div  href={blog.href} className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg">
+    <div  href={blog.href} type="button" className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg">
       {/* Display the blog title */}
       <div className="m-3 font-bold">{blog.title || 'Untitled'}</div>
 
