ea5fad81208376ce31208581066642fcbb2702f4
epicssolution <dharti25600@gmail.com>
2024-11-08 09:01:44 +0500

    Update index.js

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -3,7 +3,9 @@ import { coursesType } from "../courses"
 import { engType } from "../engType"
 import { devType } from "../devType"
 import { aiType } from "../aiType"
+import { equipmentType } from "../equipmentType"
+
 
 export const schema = {
-  types: [blogType,coursesType,engType,devType,aiType],
+  types: [blogType,equipmentType,coursesType,engType,devType,aiType],
 }
