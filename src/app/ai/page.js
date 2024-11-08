6a237af7e0ba0ede660f0681c72a81042b820ca5
epicssolution <dharti25600@gmail.com>
2024-11-08 19:19:51 +0500

    Update page.js

--- src/app/ai/page.js
+++ src/app/ai/page.js
@@ -1,17 +1,17 @@
 import React from "react";
-import Artificial from "@/components/Artificial/page";
 import Head from 'next/head'; // Importing Head component for adding meta tags
+import Artificial from "@/components/Artificial/page";
 
 export default function Artif() {
   return (
     <>
       {/* Adding the Head component for Open Graph and Twitter meta tags */}
       <Head>
-        <title>Artificial Intelligence courses & Blogs</title>
-        <meta property="og:title" content=" HVAC Equipment and Devices courses & Blogs" />
+        <title>Development courses</title>
+        <meta property="og:title" content="online courses" />
         <meta
           property="og:description"
-          content="Educational websites help students read informative blogs  and enroll in the best online courses related to use of Artificial Intelligence "
+          content="Educational websites help students gain admission to top uonline courses  and enroll in the best courses."
         />
         <meta property="og:image" content="https://www.epicssolution.com/social-banner.png" />
         <meta property="og:url" content="https://www.epicssolution.com/" />
@@ -19,10 +19,10 @@ export default function Artif() {
         
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content=" Artificial Intelligence  & Blogs" />
+        <meta name="twitter:title" content="Study Visa Consultant" />
         <meta
           name="twitter:description"
-          content="Educational websites help students read informative blogs  and enroll in the best online courses related to use of Artificial Intelligence "
+          content="Educational websites help students gain admission to top universities and enroll in the best courses."
         />
         <meta name="twitter:image" content="https://www.epicssolution.com/social-banner.png" />
       </Head>
@@ -30,11 +30,12 @@ export default function Artif() {
       <div className="mt-8">
         {/* Second component */}
         <div className="flex justify-center align-middle font-semibold text-2xl border-[1px] border-solid border-dark dark:border-light text-black dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh]" >
-          Artificial Intelligence
+       Artificial Intelligence Courses
+
         </div>
        <div className=" mt-6 "> <article  style={{ minHeight: '300px', width: '100%' }}>
-        <Artificial />
-       </article></div>
+          <Artificial />
+        </article></div>
       </div>
     </>
   );
