a66c9f27d6991138dd4a31d9b34af567b971df0b
epicssolution <dharti25600@gmail.com>
2024-11-12 06:45:56 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,7 +58,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
+        <link rel="sitemap" type="application/xml" href="https://www.epicssolution.com/sitemap.xml" />
+        <meta httpEquiv="Cache-Control" content="public, max-age=31536000, immutable" />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta property="og:updated_time" content={metadata.openGraph.updated_time} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:image" content={metadata.openGraph.images[0]} />
+        <meta name="theme-color" content="#ffffff" /> {/* Define theme color for browsers */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="last-modified" content={metadata.openGraph.updated_time} />
       <meta
           name="google-site-verification"
           content="125c3Cukk3D1INp6HOlRmuvTDPOk-qiR_j30PREvm0I"
