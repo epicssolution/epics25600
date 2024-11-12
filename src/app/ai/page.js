9b722d9cd110f0c6e1334d1bcde898833b7a7b40
epicssolution <dharti25600@gmail.com>
2024-11-12 14:04:51 +0500

    Update page.js

--- src/app/ai/page.js
+++ src/app/ai/page.js
@@ -31,7 +31,7 @@ export default function Artif() {
       <div className="mt-8">
         {/* Second component */}
         <div className="flex justify-center align-middle font-semibold text-2xl border-[1px] border-solid border-dark dark:border-light text-black dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh]" >
-       Artificial Intelligence Courses
+          <h1> Artificial Intelligence Blogs & Courses  </h1>
 
         </div>
        <div className=" mt-6 "> <article  style={{ minHeight: '300px', width: '100%' }}>
