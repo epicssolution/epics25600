faa9a21159291737753db6679aef59ca7f2f10d2
epicssolution <dharti25600@gmail.com>
2024-11-23 18:56:52 +0500

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,4 +1,4 @@
-"use client"; // Ensure this is at the top of your file
+"use client"; // Ensure this is the first statement in your file
 
 import React, { useState } from "react";
 import Link from "next/link";
