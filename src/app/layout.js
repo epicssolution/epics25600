84a7242236b5c8725071b11f988f3f351ed300bf
epicssolution <dharti25600@gmail.com>
2024-12-10 20:05:10 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,54 +58,56 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
-          rel="sitemap"
-          type="application/xml"
-          href="https://www.epicssolution.com/sitemap.xml"
-        />
-        <link rel="canonical" href="https://www.epicssolution.com/" />
-        <link rel="icon" href="https://www.epicssolution.com/favicon.ico" />
-        <link rel="preload" href="/css/main.css" as="style" />
-        <link
-          rel="preload"
-          href="/fonts/CustomFont.woff2"
-          as="font"
-          type="font/woff2"
-          crossorigin="anonymous"
-        />
+  <link
+    rel="sitemap"
+    type="application/xml"
+    href="https://www.epicssolution.com/sitemap.xml"
+  />
+  <link rel="canonical" href="https://www.epicssolution.com/" />
+  <link rel="icon" href="https://www.epicssolution.com/favicon.ico" />
+  <link rel="preload" href="/css/main.css" as="style" />
+  <link
+    rel="preload"
+    href="/fonts/CustomFont.woff2"
+    as="font"
+    type="font/woff2"
+    crossorigin="anonymous"
+  />
 
-        {/* SEO Meta Tags */}
-        <meta name="description" content={siteMetadata.description} />
-        <meta name="author" content="Epic Solutions" />
+  {/* SEO Meta Tags */}
+  <meta name="description" content={siteMetadata.description} />
+  <meta name="author" content="Epic Solutions" />
 
-        {/* Google Verification and Viewport */}
-        <meta
-          name="google-site-verification"
-          content="125c3Cukk3D1INp6HOlRmuvTDPOk-qiR_j30PREvm0I"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+  {/* Google Verification and Viewport */}
+  <meta
+    name="google-site-verification"
+    content="125c3Cukk3D1INp6HOlRmuvTDPOk-qiR_j30PREvm0I"
+  />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-       
+  {/* Google Analytics */}
+  <Script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-D50XE9PL55"
+  />
+  <Script id="gtag-init" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-D50XE9PL55');
+    `}
+  </Script>
 
-        {/* Google Analytics */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-D50XE9PL55"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-D50XE9PL55');
-          `}
-        </Script>
+  {/* Google AdSense */}
+  <Script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6106733128223559"
+    crossOrigin="anonymous"
+  />
+  <meta name="google-adsense-account" content="ca-pub-6106733128223559" />
+</head>
 
-        {/* Google AdSense */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6106733128223559"
-     crossorigin="anonymous">
-         <meta name="google-adsense-account" content="ca-pub-6106733128223559">
-      </head>
       <body
         className={cx(
           inter.variable,
