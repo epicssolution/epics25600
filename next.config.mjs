ae6e0bdda141756428733857f0e224efdea68828
epicssolution <dharti25600@gmail.com>
2025-02-06 16:00:13 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,287 +10,175 @@ const nextConfig = {
       },
     ],
   },
- async redirects() {
+  async redirects() {
     return [
-   {
-        source: '(/marketing/revit-mep-2025-tutorial-1-hvac-load-calculationRevit|/marketing/revit-mep-2025-cooling-and-heating-load-calculation-p-1|/marketing/revit-mep-2025-tutorial-1-hvac-load-calculation|/revit-2025-hvac-load-calculation-tutorial-1|/revit-2025-hvac-load-|/how-chillers-work-|/Epics|/marketing/revit-mep-2025-tutorial-1-hvac-load-calculation|/marketing/revit-mep-2025-cooling-and-heating-load-calculation|/marketing/asif-khan|/marketing/marketing-and-ai|/marketing/URL\\ to\\ favicon)',
+      {
+        source: '(?:/marketing/revit-mep-2025-tutorial-1-hvac-load-calculation(?:Revit)?|/marketing/revit-mep-2025-cooling-and-heating-load-calculation-p-1|/marketing/revit-mep-2025-tutorial-1-hvac-load-calculation|/revit-2025-hvac-load-calculation-tutorial-1|/revit-2025-hvac-load-|/how-chillers-work-|/Epics|/marketing/asif-khan|/marketing/marketing-and-ai)',
         destination: '/revit/revit-mep-tutorial-1',
         permanent: true,
       },
       {
         source: '/artificial/building-management-systems-bms',
         destination: '/building-management-systems-bms',
-        permanent: true, // 301 Permanent Redirect
-      }, 
+        permanent: true,
+      },
       {
         source: '/Engineering/fundamentals-of-energy-engineering',
         destination: '/fundamentals-of-energy-engineering',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-       {
+      {
         source: '/marketing/image-optimization-in-next-js',
         destination: '/image-optimization-in-next-js',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-          {
+      {
         source: '/development/next-js-a-full-stack-developer',
         destination: '/next-js-a-full-stack-developer',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-     {
+      {
         source: '/development/tailwind-css-revolutionize-web-development',
         destination: '/tailwind-css-revolutionize-web-development',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-        {
+      {
         source: '/development/supercharge-your-web-performance',
         destination: '/supercharge-your-web-performance',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/development/next-js-a-full-stack-developer',
-        destination: '/next-js-a-full-stack-developer',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/artificiali/applications-of-ai-in-healthcare',
         destination: '/role-of-artificial-intelligence-in-healthcare',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/Engineering/tunnel-ventilation',
         destination: '/tunnels-ventilation',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '(/Engineering/hospital-hvac-system|/hospital-hvac-system|/Engineering/hvac-system-designing-for-hospitals)',
+      {
+        source: '(?:/Engineering/hospital-hvac-system|/hospital-hvac-system|/Engineering/hvac-system-designing-for-hospitals)',
         destination: '/hospital-hvac',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/room-heating-by-perforated-solar-air-collector',
-        destination: '/room-heating-by-perforated-solar-air-collector',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/Engineering/hvac-load-calculation',
-        destination: '/hvac-load-calculation',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/Engineering/hvac-and-indoor-air-quality',
-        destination: '/hvac-and-indoor-air-quality',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/equipment/how-chiller-work',
         destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         
-        
-         {
+      {
         source: '/artificialf/ai',
         destination: '/ai',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/artificialf/building-management-systems-bms',
         destination: '/building-management-systems-bms',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/Engineering/centrifugal-liquid-chiller-operation-and-maintenance',
         destination: '/equipment',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/marketing/saifullah',
         destination: '/equipment',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-        
-         {
+      {
         source: '/marketing/tailwind-css-a-game-changer-in-web-development',
         destination: '/dev',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/artificiali/ai',
-        destination: '/ai',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/Engineering/energy-engineering',
         destination: '/revit-2025-hvac-load-calculation-tutorial-1',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/role-of-ai-in-mep-indutry',
+      {
+        source: '/Engineering/role-of-ai-in-mep-industry',
         destination: '/role-of-artificial-intelligence-in-healthcare',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/artificial/applications-of-ai-in-healthcare',
         destination: '/role-of-artificial-intelligence-in-healthcare',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/Engineering/hvac-cooling-load-calculation',
         destination: '/hvac-load-calculation',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/Engineering/chiller-operation-and-maintenance',
         destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/centrifugal-liquid-chiller',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/tunnel-ventilation',
         destination: '/tunnels-ventilation',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/hvac-',
-        destination: '/tunnels-ventilation',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/hvac-cooling-load-calculation',
         destination: '/hvac-load-calculation',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/tunnels-',
-        destination: '/tunnels-ventilation',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/Engineering/concepts-of-chiller-in-hvac',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         
-         {
+      {
         source: '/Engineering/tunnels-ventilation',
         destination: '/tunnels-ventilation',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/development/tailwind-css-a-game-changer-in-web-development',
         destination: '/tailwind-css-revolutionize-web-development',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/centrifugal-liquid-chiller',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/marketing/building-management-systems-bms',
-        destination: '//building-management-systems-bms',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/Engineering/centrifugal-liquid-chiller',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/marketing/building-management-systems-bms',
         destination: '/building-management-systems-bms',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
+      {
         source: '/equipment/how-chiller-works',
         destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-         {
-        source: '/Engineering/plumbing-design-for-people-with-disability',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/Engineering/chiller-system-greater-than-types-working-o-and-m',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
-        source: '/Engineering/chiller-details',
-        destination: '/how-chillers-work-operation-and-maintenance-tips',
-        permanent: true, // 301 Permanent Redirect
-      },
-         {
+      {
         source: '/marketing/use-of-ai-in-digital-marketing-details',
         destination: '/role-of-artificial-intelligence-in-healthcare',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-       {
+      {
         source: '/hospital-hvac-system',
         destination: '/hospital-hvac',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-   {
+      {
         source: '/development/applications-of-ai-in-healthcare',
         destination: '/role-of-artificial-intelligence-in-healthcare',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
-       {
+      {
         source: '/artificial-intelligence-',
         destination: '/role-of-artificial-intelligence-in-healthcare',
-        permanent: true, // 301 Permanent Redirect
-      },   {
-        source: '/artificialf/ai',
-        destination: '/ai',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/artificialf/building-management-systems-bms ',
-        destination: '/building-management-systems-bms',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/equipment/how-chiller-work ',
-        destination: '/equipment',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/development/tailwind-css-a-game-changer-in-web-development ',
-        destination: '/tailwind-css-revolutionize-web-development',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/Engineering/tunnels-ventilation',
-        destination: '/tunnels-ventilation',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/hvac-system-designing-for-hospitals ',
-        destination: '/hospital-hvac',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/marketing/use-of-ai-in-digital-marketing',
-        destination: '/dev',
-        permanent: true, // 301 Permanent Redirect
-      }, {
-        source: '/artificial-',
-        destination: '/ai',
-        permanent: true, // 301 Permanent Redirect
-      },{
+        permanent: true,
+      },
+      {
         source: '/high-income-skills',
         destination: '/high-income-skill',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
       {
         source: '/(data-analyses|data-visualization)',
         destination: '/data-visualization1',
-        permanent: true, // 301 Permanent Redirect
+        permanent: true,
       },
     ];
   },
-  
-  
 };
 
 export default nextConfig;
