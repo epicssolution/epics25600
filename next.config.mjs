aa5082f73c56f1a7558a9140580562ac32ca8695
epicssolution <dharti25600@gmail.com>
2025-02-06 17:35:42 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -97,7 +97,7 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: '/Engineering/role-of-ai-in-mep-industry',
+        source: '/Engineering/(role-of-ai-in-mep-industry|role-of-ai-in-mep-indutry)',
         destination: '/the-impact-of-artificial-intelligence-on-mep-systems',
         permanent: true,
       },
