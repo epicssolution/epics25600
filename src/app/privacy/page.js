8d1b77112c0c852651ac411c459a2599839a8ed4
epicssolution <dharti25600@gmail.com>
2025-02-28 17:16:03 +0500

    Update page.js

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -4,9 +4,7 @@ import Head from 'next/head';
 export default function PrivacyPolicy() {
   return (
     <div className="bg-gray-50 text-gray-800 p-6 max-w-4xl mx-auto">
-      <Head>
-        <title>Privacy Policy | Epic Solutions</title>
-      </Head>
+     
       <h1> privacy policy</h1>
     </div>
   );
